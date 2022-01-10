5ad5e272ef8f8719206531772701f49fb8d41d24
Immanuel Matthews <ilmfeemster@gmail.com>
2022-01-09 21:51:22 -0800

    add new grid functionality, still buggy

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ for (i=0; i<16; i++){
     }
 }
 let gridCell = document.querySelectorAll(".cell");
-console.log(gridCell);
 
 for (i = 0; i < gridCell.length; i++){
     gridCell[i].addEventListener("mouseover", function(event){
@@ -25,4 +24,25 @@ reset.addEventListener("click", function(){
     for (i = 0; i < gridCell.length; i++){
         gridCell[i].style.backgroundColor = "white"
         }
+    let prompt0 = prompt("Input Grid Size:");
+    let gridSize = parseInt(prompt0);
+    for (i = 0; i < gridCell.length; i++){
+        gridCell[i].remove()
+        }
+    for (i=0; i<gridSize; i++){
+        let row = document.createElement("div");
+        row.className = "row"
+        container.appendChild(row)
+        for (j = 0; j < gridSize; j++){
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            row.appendChild(cell);
+        }
+    }
+    let resetCell = document.querySelectorAll(".cell");
+    for (i = 0; i < resetCell.length; i++){
+        resetCell[i].addEventListener("mouseover", function(event){
+            event.target.style.backgroundColor = "gray"
+        })
+    }
     })
